bd8a83c06f2923b7f354405dd3aad09aeac8480e
Evgeny Khoroshukhin <horoshuhin@gmail.com>
2022-07-12 20:58:14 -0400

    added functions for scene attributes extraction

--- screenplay.py
+++ screenplay.py
@@ -1,55 +1,138 @@
-from collections import OrderedDict
+from pprint import pprint
+from dataclasses import dataclass, field
 
 
-class Screenplay():
+@dataclass
+class Screenplay:
 
-    INT_EXT = ['INT.', 'EXT.', 'INT/EXT.', 'EXT/INT.']
-    TOD = ['AFTERNOON', 'CONTINUOUS', 'DAWN', 'DAY', 'DUSK',
-           'EVENING', 'LATER', 'MORNING', 'NIGHT', 'NOON', 'SAME']
-    TRANSITIONS = ['BACK TO:', 'CUT TO:', 'DISSOLVE TO:', 'FADE IN:', 'CUT BACK TO:'
-                   'FADE OUT:', 'FADE TO:', 'JUMP CUT TO:', 'MATCH CUT TO:', 'TITLE:',
-                   'SMASH CUT TO:', 'TIME CUT:', 'WIPE TO:', 'CROSSFADE:', 'OVER BLACK:', 'CUT TO BLACK',
-                   'FADE TO:BLACK', 'FLASH CUT:', 'LAP DISSOLVE:', 'MATCH DISSOLVE TO:', 'SUPER:', 'IRIS OUT:']
-    SHOTS = ['AERIAL SHOT', 'ANGLE ON',  'CLOSE ON', 'CLOSER ANGLE', 'CONTINUOUS',
-             'CONTRAZOOM', 'CRAWL',  'ESTABLISHING SHOT', 'DAYS LATER'
-             'EXTREMELY LONG SHOT (XLS):', 'FAVOR ON',
-             'FLASHBACK:', 'FREEZE FRAME:', 'INSERT',
-             'INTO FRAME:', 'INTO VIEW:', 'MOS', 'O.C. / O.S.',
-             'POV', 'PULL BACK:', 'PULL FOCUS:', 'PUSH IN:',
-             'REVERSE ANGLE',  'SPLIT SCREEN SHOT:', 'STOCK SHOT:',
-             'TIGHT ON', 'TIME CUT', 'V.O.', 'ZOOM:']
+    text: str
 
-    def __init__(self, text: list):
-        self.text = [l.strip() for l in text if not l.isspace()]
+    result = {}
 
-    def get_scenes(self):  # list of scene
-        self.scenes = []
-        for i in [e for e in self.text if e.isupper() and e not in self.TRANSITIONS]:
+    INT_EXT: list[str] = field(default_factory=lambda: [
+                               'INT.', 'EXT.', 'INT/EXT.', 'EXT/INT.'])
+    TOD: list[str] = field(default_factory=lambda: ['AFTERNOON', 'CONTINUOUS', 'DAWN', 'DAY', 'DUSK',
+                                                    'EVENING', 'LATER', 'MORNING', 'NIGHT', 'NOON', 'SAME', 'SUNSET'])
+
+    TRANSITIONS: list[str] = field(default_factory=lambda: ['BACK TO:', 'CUT TO:', 'DISSOLVE TO:', 'FADE IN:', 'CUT BACK TO:', 'CUT BACK TO:',
+                                                            'FADE OUT:', 'FADE TO:', 'JUMP CUT TO:', 'MATCH CUT TO:', 'TITLE:', 'QUICK CUTS:',
+                                                            'SMASH CUT TO:', 'TIME CUT:', 'WIPE TO:', 'CROSSFADE:', 'OVER BLACK:', 'CUT TO BLACK',
+                                                            'FADE TO:BLACK', 'FLASH CUT:', 'LAP DISSOLVE:', 'MATCH DISSOLVE TO:', 'SUPER:', 'IRIS OUT:'])
+    SHOTS: list[str] = field(default_factory=lambda: ['AERIAL SHOT', 'ANGLE ON',  'CLOSE ON', 'CLOSER ANGLE', 'CONTINUOUS',
+                                                      'CONTRAZOOM', 'CRAWL',  'ESTABLISHING SHOT',
+                                                      'EXTREMELY LONG SHOT (XLS):', 'FAVOR ON', 'HEAR LAUGHTER.',
+                                                      'FLASHBACK:', 'FREEZE FRAME:', 'INSERT', 'DAYS LATER'
+                                                      'INTO FRAME:', 'INTO VIEW:', 'MOS', 'O.C. / O.S.',
+                                                      'POV', 'PULL BACK:', 'PULL FOCUS:', 'PUSH IN:',
+                                                      'REVERSE ANGLE',  'SPLIT SCREEN SHOT:', 'STOCK SHOT:',
+                                                      'TIGHT ON', 'TIME CUT', 'V.O.', 'ZOOM:'])
+
+    def __post_init__(self):
+        # self.text = [l for l in self.text]
+        self.text = [l.strip() for l in self.text if not l.isspace()]
+
+    def locations(self) -> list[str]:
+        locations = []
+        for i in [e.strip() for e in self.text if e.isupper() and e not in self.TRANSITIONS]:
             for item in i.split():
                 if item in self.INT_EXT:
                     scene = ' '.join([x for x in i.split()])
-                    self.scenes.append(scene)
-        return self.scenes
+                    locations.append(scene)
+        return locations
 
-    def get_scene_index(self):  # list of scene ind
-        return [i for i, l in enumerate(self.text) if l in self.get_scenes()]
+    # @property
+    def scene_index(self):
+        return [i for i, l in enumerate(
+            [e for e in self.text]) if l in self.locations()]
 
-    # def parse(self):
-    #     scenes = {}
+    def scenes(self):  # use Ordered dict
+        scene_index=self.scene_index()
+        result = {}
+        for i, _ in enumerate(scene_index, 1):
+            if scene_index[i-1] == scene_index[-1]:
+                result.update({i: {'text': self.text[scene_index[i-1]:]}})
+            else:
+                result.update(
+                    {i: {'text': self.text[scene_index[i-1]:scene_index[i]]}})
+        return result
 
-    #     for i, s in enumerate(self.get_scenes(), start=1):
-    #         scenes.update({
-    #             i: {
-    #                 f'int_ext': [l for l in s.split()][0],
-    #                 'location': ' '.join([l for l in s.split()][1:-2]),
-    #                 'tod': [l for l in s.split()][-1]
-    #             }})
+    
+    
+    
+    # def delete_phrases(self,text, phrases):
+    #     if len(phrases) == 1:
+    #         result = [t for t in text if phrases[0] not in t]
+    #         return result
+    #     else:
+    #         for idx, phrase in enumerate(phrases):
+    #             result = [t for t in text if phrase not in t]
+    #             return self.delete_phrases(result, phrases[idx+1:])
+            
+    def characters(self):
+        # scene_index=self.scene_index()
+        result = {}
+        for i, v in enumerate(self.scenes().items(), start=1):
+            # print({i:{'characters':[c for c in v[1]['text'] if c.isupper()]}})
+            result.update(
+                {i: {'characters': list(set(c for c in v[1]['text'] if c.isupper()
+                                       and c not in self.locations()
+                                       and c not in self.TRANSITIONS
+                                       and c not in self.SHOTS
+                                       and c not in self.INT_EXT
+                                       and ',' not in c
+                                       and '!' not in c
+                                       and not c.startswith('(')
+                                       and len(c) < 25))}})
+        return result
 
+    def transitions(self):
+        result = {}
+        for i, v in enumerate(self.scenes().items(), start=1):
+            result.update(
+                {i: {'transitions':[t for t in v[1]['text'] if t in self.TRANSITIONS]}})
 
+        return result
+
+        
+
+    @classmethod
+    def add_transition(cls, transition):
+        cls.TRANSITIONS.append(transition)
+
+    @classmethod
+    def add_shot(cls, shot):
+        cls.SHOTS.append(shot)
+
+    def load(self):      
+        result = {}
+        for i, s in enumerate(self.scenes().items(), start=1):
+            result.update({
+                    i:{
+                    f'int_ext':[l for l in s.split()][0],
+                    # 'location':' '.join([l for l in s.split() if l not in self.TOD and l!='-'][1:-1]),
+                    # 'tod':[l for l in s.split() if l in self.TOD][0],
+                    # 'transitions':[t for t in s[1]['text'] if t in self.TRANSITIONS]
+        #             # 'action': self.get_action()[i-1]
+                    
+                    }}) 
+        
+        return result
 if __name__ == '__main__':
     with open('text.txt', 'r') as f:
         text = f.readlines()
 
-        sc = Screenplay(text)
-        # print(sc.parse())
-        # print(sc.get_scene_index())
+    sc = Screenplay(text[:2000])
+    # print(sc.TRANSITIONS)
+    # print(sc.scenes())
+    # print(sc.locations())
+    # print(sc.scenes())
+    # pprint(sc.characters())
+    # pprint(sc.transitions())
+    pprint(sc.load())
+    # pprint([c for c in sc.scenes()[11]['text']])
+    # print([i for i in sc.scenes()[11]['text'] if i.isupper()
+    #        and i not in sc.locations()
+    #        and i not in sc.TRANSITIONS
+    #        and i not in sc.SHOTS
+    #        and ',' not in i
+    #        and len(i)<25])
